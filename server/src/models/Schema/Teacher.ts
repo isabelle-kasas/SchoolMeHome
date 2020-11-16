5f6025a393341a0bee31856984bc8d765ea99158
isabelle kasas <isabellekasas@mbp-de-isabelle.home>
2020-11-16 15:07:17 +0100

    ajout class User et shemaTeacher

--- server/src/models/Schema/Teacher.ts
+++ server/src/models/Schema/Teacher.ts
@@ -1,1 +1,14 @@
-//Shema TEacher
+import mongoose from 'mongoose';
+import User from '../Class/User';
+
+const Schema = mongoose.Schema;
+
+const TeacherSchema = new Schema({
+    user: User,
+    subject: [Subject],
+    lessons: [Lesson], 
+    promo: [Promo],
+});
+
+export = mongoose.model('TeacherModel', TeacherSchema);
+
