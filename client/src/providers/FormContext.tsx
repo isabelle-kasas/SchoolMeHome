ed52dee7507579bd3c864c15f03e6d9458f54906
vbouault <vbouault@hotmail.fr>
2020-11-18 10:19:04 +0100

    delete context

--- client/src/providers/FormContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useState } from "react";
-
-export const FormContext = createContext(null);
-
-export const FormContextProvider = ({ children }: any) => {
-    const [intervenantSelected , setIntervenantSelected ] = useState({});
-
-    return <FormContext.Provider value={{ intervenantSelected , setIntervenantSelected }}>{children}</FormContext.Provider>;
-}
