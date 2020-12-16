844f888e1a516b4b1ce847290eabdf84ab971d72
Cbeterous <celine.beterous@gmail.com>
2020-12-16 18:25:15 +0100

    background

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -10,30 +10,33 @@ export function Navbar() {
       <nav className="navbar">
         <ul className="navbar-nav">
           <li className="avatar">
-            <img src={avatar} alt="avatar" className="avatarImg"/>
-            <div className="avatarName">John Doe</div>
+            <div className='nav-link'>
+              <img src={avatar} alt="avatar" className="avatarImg"/>
+              <div className="avatarName">John Doe</div>
+            </div>
+            
           </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
-              <MdDashboard className="fa-primary"/>
+              <MdDashboard size={30}/>
               <span className="link-text">Mon dashboard</span>
             </a>
             </li>
           <li className="nav-item">
             <a href="/" className="nav-link">
-              <FaUser className="fa-primary"/>
+              <FaUser size={30}/>
               <span className="link-text">Mon profil</span>
             </a>
           </li >
             <li className="nav-item">
               <a href="/" className="nav-link">
-              <FaBook className="fa-primary"/>
+              <FaBook size={30}/>
               <span className="link-text">Mes cours</span>
             </a>
             </li>
             <li className="nav-item">
                <a href="/" className="nav-link">
-              <FaSignOutAlt className="fa-primary"/>
+              <FaSignOutAlt size={30}/>
               <span className="link-text">Se déconnecter</span>
             </a>
             </li>
