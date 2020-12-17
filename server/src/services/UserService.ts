2881f366771cd75f9fbe36b4c88258271cf9ac06
isabelle kasas <isabellekasas@mbp-de-isabelle.home>
2020-12-17 20:53:51 +0100

    déplacement de la méthode signin dans un service d'authentification

--- server/src/services/UserService.ts
+++ server/src/services/UserService.ts
@@ -2,6 +2,7 @@ import User from "../models/Schema/User";
 import * as argon from 'argon2'
 import UserClass from "../models/Class/User";
 import * as jwt from 'jsonwebtoken';
+import {Auth}  from "./auth.service";
 
 
 class UserServiceClass{
@@ -12,31 +13,6 @@ class UserServiceClass{
         const user = new User(newUser);
         await user.save();
         return user;    
-    };
-
-    async signin(email: string, password: string) {
-        const user = await this.findByEmail(email);
-        if(!user){
-            return "nope"
-        }
-        const valid = await argon.verify(user.password, password);
-        if(!valid){
-            return 'nope';
-        }
-        const userToken = {
-            email: user.email,
-            lastName: user.lastName,
-            firstName: user.firstName
-        };
-
-        // const secret = process.env.JWT_SECRET;
-        const token = jwt.sign(userToken, "secret");
-         return {
-            token,
-            email: user.email,
-            lastName: user.lastName,
-            firstName: user.firstName
-        };
     };
     
     async getAll(){
