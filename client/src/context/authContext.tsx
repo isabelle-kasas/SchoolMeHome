e460d634c6a1ab09953f350c6cc3e088c95abdc7
vbouault <vbouault@hotmail.fr>
2021-01-06 12:48:11 +0100

    passage de axios a apollo client dans login

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState, FormEvent, useContext, Dispatch } from 'react'
+import React, { createContext, useState, FormEvent, useContext, Dispatch, useEffect } from 'react'
 import axios from 'axios'
+import { gql, useMutation } from '@apollo/client'
 
 export const AuthContext = createContext<any>({});
 
@@ -10,28 +11,48 @@ export function AuthProvider({ children }: { children: JSX.Element }) {
   const [firstName, setFirstName] = useState<string>('')
   const [lastName, setLastName] = useState<string>('')
   const [alert, setAlert] = useState<boolean>(false)
-  return <AuthContext.Provider value={{ token, setToken, email, setEmail, password, setPassword, firstName, setFirstName,lastName, setLastName, alert, setAlert }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ token, setToken, email, setEmail, password, setPassword, firstName, setFirstName, lastName, setLastName, alert, setAlert }}>{children}</AuthContext.Provider>;
 }
 
 export const useAuth = () => {
 
-  const { token, setToken, email, setEmail, password, setPassword, firstName, setFirstName,lastName, setLastName, alert, setAlert } = useContext(AuthContext)
+  const { token, setToken, email, setEmail, password, setPassword, firstName, setFirstName, lastName, setLastName, alert, setAlert } = useContext(AuthContext)
 
-  const formSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    try {
-      const result = await axios.post(`${process.env.REACT_APP_API_URL}/api/signin`, { email, password });
-      if (result.data.token) {
-        setToken(result.data.token)
-        setFirstName(result.data.firstName)
-        setLastName(result.data.lastName)
-        localStorage.setItem('authToken', result.data.token)
-      } else {
-        setAlert(true)
+  const SIGNIN = gql`
+  mutation Signin (
+    $email: String!
+    $password: String!
+  ) {
+    signin(email: $email, password: $password){
+      token
+      user {
+        firstName
+        lastName
       }
-    } catch (error) {
-      console.error(error)
     }
+  }
+`;
+
+  const [signin] = useMutation<any>(SIGNIN);
+
+
+  const formSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    signin({ variables: { email: email, password: password } })
+      .then((data) => {
+        const result = data.data.signin
+        if (result) {
+          setToken(result.token)
+          setFirstName(result.user.firsName)
+          setLastName(result.user.lastName)
+          localStorage.setItem('authToken', result.token)
+        } else {
+          setAlert(true)
+        }
+      })
+      .catch(err => console.error(err))
+
   }
 
   const disconnect = () => {
@@ -64,7 +85,7 @@ export const useAuth = () => {
     formSubmit,
     firstName,
     lastName,
-    disconnect, 
+    disconnect,
     alert,
     handleCloseMui
   }
