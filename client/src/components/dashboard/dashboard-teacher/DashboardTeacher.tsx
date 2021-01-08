efd95e8c92862512ad488cf111eb9f74dc4b7c88
Cbeterous <celine.beterous@gmail.com>
2021-01-08 10:52:22 +0100

    dashboard teacher

--- client/src/components/dashboard/dashboard-teacher/DashboardTeacher.tsx
+++ client/src/components/dashboard/dashboard-teacher/DashboardTeacher.tsx
@@ -1,10 +1,82 @@
+import { CardActionArea, CardContent, CardMedia, createStyles } from '@material-ui/core';
+import { Grid, makeStyles, Card, Theme } from '@material-ui/core'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './DashboardTeacher.css'
+import dashboardCours from '../../../image/dashboard-cours.png';
+import dashboardPromo from '../../../image/dashboard-promo.png';
+import dashboardNextCours from '../../../image/dashboard-nextCours.png';
+import dashboardAdmin from '../../../image/dashboard-admin.png';
+
 
 const DashboardTeacher = () => {
   return (
-    <div>
-      <Link to="/creation">Créer un cour</Link>
+    <div >
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="flex-start"
+        spacing={4}
+      >
+        <Grid item xs={4}>
+          <Card elevation={3} className="blue" >
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image={dashboardNextCours}
+                  title="Prochain cours"
+                  className="nextcours"
+                />
+
+               Mes Prochains Cours
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card elevation={3} className="green">
+
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image={dashboardCours}
+                  title="Créer un cours"
+                  className="cours"
+                />
+
+                <Link to="/creation" className="link">Créer un cour</Link>
+              </CardContent>
+
+          </Card>
+          <Card elevation={3} className="green">
+            <CardContent>
+              <CardMedia
+                component="img"
+                image={dashboardPromo}
+                title="Mes promotions"
+                className="promo"
+              />
+               Mes Promotions
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card elevation={3} className="blue">
+              <CardContent>
+                <CardMedia
+                  component="img"
+                  image={dashboardAdmin}
+                  title="Administration"
+                  className="admin"
+                />
+
+               Administration
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
+
     </div>
   )
 }
